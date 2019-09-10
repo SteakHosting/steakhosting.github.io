45cb158fe18153d3f6fcbcb7f6a842a50562c912
SteakHosting <55145054+SteakHosting@users.noreply.github.com>
2019-09-10 21:34:57 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// If you don't want the particles, remove the code below!
+// Do not delete this file unless you know what you're doing.
+
+$.firefly({
+	minPixel: 1,
+	maxPixel: 3,
+	total: 50
+});
